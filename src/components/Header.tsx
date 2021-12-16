66d1053c3e62d1c4cf44c002656c609375d85381
thuan <thuan.hoang@vastbit.com>
2021-12-16 12:36:55 +0700

    refactor 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { ic_close } from '../assets/icons';
 import { IconButton } from './IconButton';
 import { colors } from '../constants/colors';
-import { heightPixel, pixelSizeHorizontal } from '../utils/scaling';
+import { defaultStyle } from '../constants/defaultStyle';
+import { fontPixel, heightPixel, pixelSizeHorizontal, widthPixel } from '../utils/scaling';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const Header = ({ onPress }) => {
+const Header = ({ onPress, concurrent = 0 }) => {
     const { bottom } = useSafeAreaInsets();
     return (
         <View style={[styles.contain, { top: bottom + heightPixel(16) }]}>
+            <View style={styles.wrapConcurrent}>
+                <Text style={[defaultStyle.subButton, { color: colors.WHITE }]}>{concurrent}</Text>
+            </View>
             <IconButton icon={ic_close} onPress={onPress} tintColor={colors.light.White} />
         </View>
     );
@@ -23,5 +27,14 @@ const styles = StyleSheet.create({
         zIndex: 999,
         alignSelf: 'flex-end',
         paddingHorizontal: pixelSizeHorizontal(16),
+        ...defaultStyle.flexRow,
+    },
+    wrapConcurrent: {
+        backgroundColor: colors.light.DUSTY_GRAY,
+        padding: fontPixel(4),
+        borderRadius: fontPixel(10),
+        marginHorizontal: pixelSizeHorizontal(8),
+        minWidth: widthPixel(30),
+        alignItems: 'center',
     },
 });
