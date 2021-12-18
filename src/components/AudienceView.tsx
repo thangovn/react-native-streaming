f4a3e14aa0eb21829f6d205d159b693365a199d4
thuan <thuan.hoang@vastbit.com>
2021-12-18 17:41:12 +0700

    add

--- src/components/AudienceView.tsx
+++ src/components/AudienceView.tsx
@@ -2,7 +2,6 @@ import { IndicatorLoading } from './IndicatorLoading';
 import { colors } from '../constants/colors';
 import { defaultStyle } from '../constants/defaultStyle';
 import { HEIGHT_SCREEN, WIDTH_SCREEN } from '../constants/spacing';
-import { map } from 'lodash';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ConnectionStateType, RtcRemoteView, VideoRenderMode } from 'react-native-agora';
@@ -21,16 +20,13 @@ const AudienceView = ({ onClose, connection, concurrent, peerIds, channelName })
             {connection === ConnectionStateType.Connecting ? (
                 <IndicatorLoading backgroundColor={colors.light.MIRAGE} />
             ) : connection === ConnectionStateType.Connected && Boolean(peerIds.length) ? (
-                map(peerIds, (value: number) => (
-                    <RtcRemoteView.SurfaceView
-                        style={styles.remoteView}
-                        key={value}
-                        uid={value}
-                        channelId={channelName}
-                        renderMode={VideoRenderMode.Hidden}
-                        zOrderMediaOverlay={true}
-                    />
-                ))
+                <RtcRemoteView.SurfaceView
+                    style={styles.remoteView}
+                    uid={peerIds[0]}
+                    channelId={channelName}
+                    renderMode={VideoRenderMode.Hidden}
+                    zOrderMediaOverlay={true}
+                />
             ) : connection === ConnectionStateType.Disconnected ? (
                 <View style={styles.flex}>
                     <Text style={styles.failText}>{'Stream Disconnected'}</Text>
